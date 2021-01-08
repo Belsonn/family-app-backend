344aa32fff0ec577fcd38235f0f29ad87a7e581e
Mateusz Belka <mateusz.belka14@gmail.com>
2021-01-08 22:24:30 +0100

    poprawki w kalendarzu, zmienione endpointy, dodano ikony

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -3,18 +3,7 @@ const Event = require("./../models/event.model");
 const Family = require("./../models/family.model");
 
 exports.addEvent = async (req, res, next) => {
-  //   const event = {
-  //     name: req.body.name,
-  //     color: req.body.color,
-  //     eventType: req.body.type,
-  //     users: req.body.users,
-  //     startDate: req.body.startDate,
-  //     endDate: req.body.endDate,
-  //     allDay: req.body.allDay,
-  //     repeatType: req.body.repeatType,
-  //     repeatEvery: req.body.repeatEvery,
-  //   };
-
+  
   const event = await Event.create(req.body);
 
   family = await Family.findByIdAndUpdate(
@@ -39,17 +28,18 @@ exports.getEvents = async (req, res, next) => {
   if (!family) {
     return next(new globalError("There is no familyID", 404));
   }
-
   let events = family.events;
 
-  console.log(family.events.length)
+  events.sort((a, b) => {
+    return new Date(a.startDate).getTime() - new Date(b.startDate).getTime();
+  });
 
-  
+
   res.status(200).json({
     status: "success",
     results: family.events.length,
     data: {
-      events: family.events
+      events
     },
   });
 };
