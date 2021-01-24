b83bc1e775e8ba2e24022c19c7d1930d59c347b6
Mateusz Belka <mateusz.belka14@gmail.com>
2021-01-24 04:14:38 +0100

    dodano funkcje obslugujaca dodawanie dailyTaskow

--- controllers/dailyTaskController.js
+++ controllers/dailyTaskController.js
@@ -1,5 +1,7 @@
 const DailyTask = require("./../models/dailyTask.model");
 const Family = require("./../models/family.model");
+const Task = require("./../models/task.model");
+const globalError = require("./../utils/globalError");
 
 exports.createDailyTask = async (req, res, next) => {
   const dailyTask = await DailyTask.create(req.body);
@@ -26,6 +28,14 @@ exports.getDailyTasks = async (req, res, next) => {
 
   let dailyTasks = family.dailyTasks;
 
+  dailyTasks.sort((a, b) => {
+    if(parseInt(a.startTime.split(":")[0]) !== parseInt(b.startTime.split(":")[0])){
+      return parseInt(a.startTime.split(":")[0]) - parseInt(b.startTime.split(":")[0]);
+    } else {
+      return parseInt(a.startTime.split(":")[1]) - parseInt(b.startTime.split(":")[1]);
+    }
+  });
+
   res.status(200).json({
     status: "success",
     results: family.dailyTasks.length,
@@ -37,8 +47,6 @@ exports.getDailyTasks = async (req, res, next) => {
 
 exports.taskOnDate = async (req, res, next) => {
   const date = new Date(req.query.date);
-
-  console.log(date);
 
   const family = await Family.findById(req.family._id).populate("tasks");
 
@@ -66,4 +74,67 @@ exports.taskOnDate = async (req, res, next) => {
   });
 };
 
-exports.createMultipleTask = async (req, res, next) => {};
+exports.updateDailyTask = async (req, res, next) => {
+  if (!req.query.date) {
+    return next(new globalError("No date provided", 400));
+  }
+  if (!req.body.tasks) {
+    return next(new globalError("No tasks provided", 400));
+  }
+
+  const date = new Date(req.query.date);
+
+  const newTasks = req.body.tasks;
+
+  // delete tasks on date
+
+  let family = await Family.findById(req.family._id).populate("tasks");
+
+  allTasks = family.tasks;
+
+  let tasks = [];
+
+  allTasks.forEach((el) => {
+    let dateLocal = new Date(el.startDate);
+
+    if (
+      el.dailyTask &&
+      date.getDate() === dateLocal.getDate() &&
+      date.getMonth() === dateLocal.getMonth() &&
+      date.getFullYear() === dateLocal.getFullYear()
+    ) {
+      tasks.push(el);
+    }
+  });
+
+  tasks.forEach(async (el) => {
+    family = await Family.findByIdAndUpdate(
+      req.family._id,
+      {
+        $pull: { tasks: el._id },
+      },
+      { new: true }
+    );
+
+    let task = await Task.findByIdAndDelete(el._id);
+  });
+
+  // add new tasks
+
+  newTasks.forEach(async (task) => {
+    const newTask = await Task.create(task);
+
+    family = await Family.findByIdAndUpdate(
+      req.family._id,
+      { $push: { tasks: newTask._id } },
+      { new: true }
+    );
+  });
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      tasks: newTasks,
+    },
+  });
+};
--- routes/task.router.js
+++ routes/task.router.js
@@ -10,6 +10,7 @@ router.post("/addTask", taskController.addTask);
 router.get("/", taskController.getTasks);
 
 router.post("/addDailyTask", dailyTaskController.createDailyTask);
+router.post("/updateDailyTasks", dailyTaskController.updateDailyTask);
 router.get("/dailyWithTask", dailyTaskController.taskOnDate);
 router.get("/daily", dailyTaskController.getDailyTasks);
 
