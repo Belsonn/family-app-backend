8e70cf84635859f43ba0fa0783fc7cfa3892f825
Mateusz Belka <mateusz.belka14@gmail.com>
2021-01-28 21:11:33 +0100

    Back - metody get i update dla rewarda

--- controllers/rewardController.js
+++ controllers/rewardController.js
@@ -5,6 +5,41 @@ const globalError = require("./../utils/globalError");
 
 const mongoose = require("mongoose");
 
+exports.checkIfRewardExistsAndAllow = async (req, res, next) => {
+  let allow = false;
+
+  if (!mongoose.isValidObjectId(req.params.id)) {
+    return next(new globalError("This is not valid ID", 404));
+  }
+
+  const reward = await Reward.findOne({ _id: req.params.id });
+
+  const family = await Family.findById(req.family._id).populate("rewards");
+
+  if (!reward || !family.rewards) {
+    return next(new globalError("This reward does not exists", 404));
+  }
+  family.rewards.forEach((el) => {
+    el._id == req.params.id ? (allow = true) : null;
+  });
+
+  if (!allow) {
+    return next(new globalError("You are not allowed to do that", 401));
+  }
+  next();
+};
+
+exports.getReward = async (req, res, next) => {
+  const reward = await Reward.findById(req.params.id);
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      reward,
+    },
+  });
+};
+
 exports.getRewardsBasic = async (req, res, next) => {
   const family = await Family.findById(req.family._id).populate("rewards");
 
@@ -60,6 +95,19 @@ exports.createReward = async (req, res, next) => {
 
   res.status(200).json({
     status: "success",
+    data: {
+      reward,
+    },
+  });
+};
+
+exports.updateReward = async (req, res, next) => {
+  const reward = await Reward.findByIdAndUpdate(req.params.id, req.body, {
+    new: true,
+  });
+
+  res.status(200).json({
+    data: "success",
     data: {
       reward,
     },
--- routes/reward.router.js
+++ routes/reward.router.js
@@ -5,9 +5,16 @@ const router = express.Router();
 
 router.use(authController.protect);
 
+router
+  .route("/reward/:id")
+  .get(rewardController.checkIfRewardExistsAndAllow, rewardController.getReward)
+  .patch(
+    rewardController.checkIfRewardExistsAndAllow,
+    rewardController.updateReward
+  );
+
 router.get("/basic", rewardController.getRewardsBasic);
 router.get("/unlocked", rewardController.getRewardsUnlocked);
 router.post("/createReward", rewardController.createReward);
-
 
 module.exports = router;
