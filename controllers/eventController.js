f4e6226943f06213b7587f699aa1f056ab777d63
Mateusz Belka <mateusz.belka14@gmail.com>
2021-02-03 23:28:49 +0100

    dodano crud do kalendarza, zmiana widoku eventów

--- controllers/eventController.js
+++ controllers/eventController.js
@@ -1,9 +1,34 @@
 const globalError = require("./../utils/globalError");
 const Event = require("./../models/event.model");
 const Family = require("./../models/family.model");
+const mongoose = require("mongoose");
+
+
+exports.checkIfEventExistsAndAllow = async (req, res, next) => {
+  let allow = false;
+
+  if (!mongoose.isValidObjectId(req.params.id)) {
+    return next(new globalError("This is not valid ID", 400));
+  }
+
+  const event = await Event.findOne({ _id: req.params.id }).populate("events");
+
+  const family = await Family.findById(req.family._id).populate("events");
+
+  if (!event || !family.events) {
+    return next(new globalError("This event does not exists", 404));
+  }
+  family.events.forEach((el) => {
+    el._id == req.params.id ? (allow = true) : null;
+  });
+
+  if (!allow) {
+    return next(new globalError("You are not allowed to do that", 401));
+  }
+  next();
+};
 
 exports.addEvent = async (req, res, next) => {
-  
   const event = await Event.create(req.body);
 
   family = await Family.findByIdAndUpdate(
@@ -12,7 +37,7 @@ exports.addEvent = async (req, res, next) => {
       $push: { events: event._id },
     },
     { new: true }
-  ).populate("events")
+  ).populate("events");
 
   res.status(200).json({
     status: "success",
@@ -23,7 +48,7 @@ exports.addEvent = async (req, res, next) => {
 };
 
 exports.getEvents = async (req, res, next) => {
-  const family = await Family.findById(req.family._id).populate("events")
+  const family = await Family.findById(req.family._id).populate("events");
 
   let events = family.events;
 
@@ -31,12 +56,46 @@ exports.getEvents = async (req, res, next) => {
     return new Date(a.startDate).getTime() - new Date(b.startDate).getTime();
   });
 
-
   res.status(200).json({
     status: "success",
     results: family.events.length,
     data: {
-      events
+      events,
     },
   });
+};
+
+exports.getEvent = async (req, res, next) => {
+  const event = await Event.findById(req.params.id);
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      event,
+    },
+  });
+};
+
+exports.updateEvent = async (req, res, next) => {
+  const event = await Event.findByIdAndUpdate(req.params.id, req.body, { new: true });
+
+  res.status(200).json({
+    status: "success",
+    data: {
+      event,
+    },
+  });
+};
+
+exports.deleteEvent = async (req, res, next) => {
+  const event = await Event.findByIdAndDelete(req.params.id);
+
+  const family = await Family.findByIdAndUpdate(req.family._id, {
+    $pull: { events: req.params.id },
+  });
+
+  res.status(204).json({
+    status: "success",
+    data: null
+  })
 };
--- routes/event.router.js
+++ routes/event.router.js
@@ -6,6 +6,13 @@ const router = express.Router();
 router.use(authController.protect);
 
 router.post("/addEvent", eventController.addEvent);
+router
+  .route("/event/:id")
+  .all(eventController.checkIfEventExistsAndAllow)
+  .get(eventController.getEvent)
+  .patch(eventController.updateEvent)
+  .delete(eventController.deleteEvent);
+
 router.get("/", eventController.getEvents);
 
 module.exports = router;
